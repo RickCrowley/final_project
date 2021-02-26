7b71c13dc1df0a216f9ff3b52478337b7bb20df6
RickCrowley <rick.crowley@kellogg.northwestern.edu>
2021-02-26 12:02:04 -0600

    firebase config added

--- netlify/functions/firebase.js
+++ netlify/functions/firebase.js
@@ -1,7 +1,14 @@
 const firebase = require("firebase/app")
 require("firebase/firestore")
 
-const firebaseConfig = {} // replace
+const firebaseConfig = {
+  apiKey: "AIzaSyAK9jD229sqFGTPk4O67FVMv6XYSVdz5Wo",
+  authDomain: "final-project-7c52c.firebaseapp.com",
+  projectId: "final-project-7c52c",
+  storageBucket: "final-project-7c52c.appspot.com",
+  messagingSenderId: "329431634394",
+  appId: "1:329431634394:web:bda5a743a2f0913813b0e3"
+} // replace
 
 if (!firebase.apps.length) {
   firebase.initializeApp(firebaseConfig)
