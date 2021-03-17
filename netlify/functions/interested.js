8ddd2d599d559a5ec237f4dc563f5307e03e4517
RickCrowley <rick.crowley@kellogg.northwestern.edu>
2021-03-17 07:30:02 -0500

    fixed interested button

--- netlify/functions/interested.js
+++ netlify/functions/interested.js
@@ -2,7 +2,7 @@ let firebase = require('./firebase')
 //everything below should be modeled off of add to create a firebase collection for interested
 exports.handler = async function (event) {
   let db = firebase.firestore()
-  console.log(event)
+  // console.log(event)
   let body = JSON.parse(event.body)
   let userId = body.userId
   let postCategory = body.category
@@ -10,8 +10,10 @@ exports.handler = async function (event) {
   let postUsername = body.username
   let postDescription = body.description
   let postValue = body.value
+  let browsePostId = body.postId 
 
   let newPost = {
+    origPostId: browsePostId,
     userId: userId,
     username: postUsername,
     imageUrl: postImageUrl,
@@ -21,11 +23,30 @@ exports.handler = async function (event) {
     created: firebase.firestore.FieldValue.serverTimestamp()
   }
 
-  let docRef = await db.collection(`interested`).add(newPost)
-  newPost.id = docRef.id
+  // console.log(`${userId}-${browsePostId}`)
+  // let currentUser = firebase.auth().currentUser
+  // console.log(currentUser.uid)
 
-  return {
-    statusCode: 200,
-    body: JSON.stringify(newPost)
+  let querySnapshot = await db.collection('interested').where('userId', '==', userId).where('origPostId', '==', browsePostId).get()
+  console.log(querySnapshot.size)
+
+  if(querySnapshot.size < 1) {
+
+    let docRef = await db.collection(`interested`).add(newPost)
+    newPost.id = docRef.id
+  
+    return {
+      statusCode: 200,
+      body: JSON.stringify(newPost)
+    }    
+
+  } else {
+
+    return { statusCode: 403 }
+
+    
+
   }
+
+  
 }
